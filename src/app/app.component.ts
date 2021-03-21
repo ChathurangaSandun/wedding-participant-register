b47941b33bed4a20523dbe96286de36d86679dd0
Chathuranga Sandun <wacsk19921002@gmail.com>
2021-03-21 14:23:46 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wedding-participant-register';
+  value: string = '美食';
+  autoFocus = {
+    focusValue: true
+  };
+  focusObj = {
+    focusValue: false,
+    date: new Date()
+  };
+
+  constructor(private _element: ElementRef, private _renderer: Renderer2) {}
+
+  change($event) {
+    console.log($event, 'onChange');
+  }
+
+  submit(value) {
+    console.log(value, 'onSubmit');
+  }
+
+  clear(value) {
+    console.log(value, 'onClear');
+  }
+
+  focus() {
+    console.log('onFocus');
+  }
+
+  blur() {
+    console.log('onBlur');
+  }
+
+  cancel() {
+    console.log('onCancel');
+  }
+
+  handleClick() {
+    this.focusObj = {
+      focusValue: true,
+      date: new Date()
+    };
+  }
 }
