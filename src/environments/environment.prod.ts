90b3517ec07689dda0ada7bc7ae590245c2a5284
Chathuranga Sandun <wacsk19921002@gmail.com>
2021-03-25 12:13:06 +0300

    assed pwa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD0iG1mOwhlE7iFLjtsCL_b8T_n37KQmpQ',
+    authDomain: 'wedding-participant-regi-900dc.firebaseapp.com',
+    databaseURL: 'https://wedding-participant-regi-900dc-default-rtdb.firebaseio.com',
+    projectId: 'wedding-participant-regi-900dc',
+    storageBucket: 'wedding-participant-regi-900dc.appspot.com',
+    messagingSenderId: '783423162047',
+    appId: '1:783423162047:web:821dd1d19cd39aed3ae80a'
+  }
 };
